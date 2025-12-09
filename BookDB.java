75049349f114653ea54a9898d5297b598d5fc9cb
JJH <jhjung100877@gmail.com>
2025-12-10 05:52:44 +0900

    .

--- BookDB.java
+++ BookDB.java
@@ -4,7 +4,7 @@ import java.util.*;
  * BookDB 클래스의 설명을 작성하세요.
  *
  * @author (2022320014_정재헌, 2022320035_배우준, 20220320018_이성민)
- * @version (2025.12.08)
+ * @version (2025.12.10)
  */
 public class BookDB
 {
--- Loan.java
+++ Loan.java
@@ -3,19 +3,19 @@
  * Loan 클래스의 설명을 작성하세요.
  *
  * @author (2022320014_정재헌, 2022320035_배우준, 20220320018_이성민)
- * @version (2025.12.08)
+ * @version (2025.12.10)
  */
 public class Loan
 {
     private Book loanedBook;
     private Borrower borrower;
-    
     public Loan(Book b, Borrower u){
         this.loanedBook = b;
         this.borrower = u;
         
         b.setLoan(this);
         u.setLoan(this);
+        
     }
     
     public String getBookID(){
